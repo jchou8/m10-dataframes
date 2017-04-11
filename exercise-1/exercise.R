0032df10fb7f26fbce0d697614431a16b28c24d9
Joseph Chou <joseph98c@gmail.com>
2017-04-11 13:44:07 -0700

    worked on ex. 1 and 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,22 @@
 
 # Create a vector of the number of points the Seahawks scored the first 4 games of last season
 # Hint: (google "Seahawks scores 2016")
+points <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+opponent.points <- c(10, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
-
+games <- data.frame(points, opponent.points)
 
 # Create a new column "diff" that is the difference in points
-
+games$diff <- abs(games$points - games$opponent.points)
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+games$won <- games$points > games$opponent.points
 
 # Create a vector of the opponents
-
+opponents <- c("Dolphins", "Rams", "49ers", "Jets")
 
 # Assign your dataframe rownames of their opponents
+rownames(games) <- opponents
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,43 +1,44 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- c(paste("Employee", 1:100))
 
 # Create a vector of 2014 salaries using the runif function
-
+salaries.2014 <- runif(100) * 80000 + 10000
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salaries.2015 <- salaries.2014 + runif(100) * 2000 - 500
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, salaries.2014, salaries.2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries$salaries.2015 - salaries$salaries.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+employee.57.2015.salary <- salaries[57, 'salaries.2015']
 
 # How many employees got a raise?
-
+num.raises <- sum(salaries$got.raise)
 
 # What was the value of the highest raise?
-
+highest.raise <- max(salaries$raise)
 
 # What was the name of the employee who recieved the highest raise?
-
+highest.raise.name <- salaries[salaries$raise == highest.raise, 'employees']
 
 # What was the largest decrease in salaries between the two years?
-
+highest.decrease <- min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
+highest.decrease.name <- salaries[salaries$raise == highest.decrease, 'employees']
 
 
 # What was the average salary increase?
-
+average.increase <- sum(salaries$raise) / nrow(salaries)
 
 ### Bonus ###
 
